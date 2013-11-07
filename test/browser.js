9126326e78a51daf5bd328ccbd982f91085d7cb2
Feross Aboukhadijeh <feross@feross.org>
2013-11-07 12:42:28 -0800

    add test: UDP client connect (for #2)

--- test/browser.js
+++
@@ -1,8 +0,0 @@
-var chromeSocket = require('../')
-
-var socket = new chromeSocket.UDPSocket()
-socket.on('data', function (data, host, port) {
-  alert(host + port)
-})
-
-socket.sendTo('hey', '127.0.0.1', 8912)
